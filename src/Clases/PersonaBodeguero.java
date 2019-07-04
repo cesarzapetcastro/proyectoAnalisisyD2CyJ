1f7846b9f21d8d04334e86c9316c7cd564b9cfda
cesar_zapet <cesarzapet@hotmail.com>
2019-07-03 21:39:51 -0600

    tercera version codificacion patron factory persona

---
+++ src/Clases/PersonaBodeguero.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author MicroSistemas
+ */
+public class PersonaBodeguero extends Persona{
+    
+    public PersonaBodeguero(String nombres, String Apellidos, int edad, int telefono, String direccion) {
+        super(nombres, Apellidos, edad, telefono, direccion);
+    }
+    
+}
---
+++ src/Clases/PersonaFactory.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import Interfaces.PersonaFactoryMethod;
+
+
+
+/**
+ *
+ * @author Jorge
+ */
+public class PersonaFactory implements PersonaFactoryMethod{
+
+    @Override
+    public Persona crearPersona(String nombres, String Apellidos, int edad, int telefono, String direccion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   
+    
+}
