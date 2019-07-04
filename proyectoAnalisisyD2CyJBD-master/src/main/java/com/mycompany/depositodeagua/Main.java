2647725d94889bcd0550af1e4944a830c9f61c8d
jorgelopezmadrid <jorgemadrid868@gmail.com>
2019-07-03 23:34:27 -0600

    se agrego conexion a mysql

---
+++ proyectoAnalisisyD2CyJBD-master/src/main/java/com/mycompany/depositodeagua/Main.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.depositodeagua;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+
+
+
+/**
+ *
+ * @author MicroSistemas
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PersonaFactoryMethod fabrica = new PersonaFactory ();
+        Persona persona = fabrica.crearPersona("cesar", "zapet", 0, 0, "direccion",2);
+        System.out.println(persona.getDescripcion());
+        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("depositoagua");
+        EntityManager em = emf.createEntityManager();
+        
+        TypedQuery<Administradorsql> queryadmin = em.createNamedQuery("Administradorsql.findAll", Administradorsql.class);
+        List<Administradorsql> listaAdmin = queryadmin.getResultList();
+
+        TypedQuery<Bodeguerosql> querybodeguero = em.createNamedQuery("Bodeguerosql.findAll", Bodeguerosql.class);
+        List<Bodeguerosql> listaBodeguero = querybodeguero.getResultList();
+        
+        TypedQuery<Vendedorsql> queryvendedor = em.createNamedQuery("Vendedorsql.findAll", Vendedorsql.class);
+        List<Vendedorsql> listaVendedor = queryvendedor.getResultList();
+        
+        for (Administradorsql tp : listaAdmin) {
+            System.out.println(tp);
+        }
+        for (Bodeguerosql tp : listaBodeguero) {
+            System.out.println(tp);
+        }
+        for (Vendedorsql tp : listaVendedor) {
+            System.out.println(tp);
+        }
+        em.close();
+        ;
+    }
+    
+}
---
+++ proyectoAnalisisyD2CyJBD-master/src/main/java/com/mycompany/depositodeagua/Persona.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.depositodeagua;
+
+/**
+ *
+ * @author MicroSistemas
+ */
+public abstract class Persona {
+    private String nombres;
+    private String Apellidos;
+    private int edad;
+    private int telefono;
+    private String direccion;
+    private int rango;
+
+    public int getRango() {
+        return rango;
+    }
+
+    public void setRango(int rango) {
+        this.rango = rango;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String Apellidos) {
+        this.Apellidos = Apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Persona(String nombres, String Apellidos, int edad, int telefono, String direccion,int rango) {
+        setNombres(nombres);
+        setApellidos(Apellidos);
+        setEdad(edad);         
+        setTelefono(telefono);
+        setDireccion(direccion);   
+        setRango(rango);
+
+    }
+    public abstract String getDescripcion();
+
+
+    }
+
+    
+
